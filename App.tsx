fca3fb2b1738cc90ee05cbffe80dc3e1a8258214
Guilherme Brida <guilherme.brida@crearesistemas.com.br>
2024-10-15 13:05:34 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,118 @@
+import React, { useState } from "react";
+import {
+  Button,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import DeviceModal from "./DeviceConnectionModal";
+import { PulseIndicator } from "./PulseIndicator";
+import useBLE from "./useBLE";
+
+const App = () => {
+  const {
+    requestPermissions,
+    scanForPeripherals,
+    allDevices,
+    connectToDevice,
+    connectedDevice,
+    heartRate,
+    disconnectFromDevice,
+    sendCommandToDevice
+  } = useBLE();
+
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+
+  const scanForDevices = async () => {
+    const isPermissionsEnabled = await requestPermissions();
+    if (isPermissionsEnabled) {
+      console.log('iniciando scan');
+      scanForPeripherals();
+    }
+  };
+
+  const hideModal = () => {
+    setIsModalVisible(false);
+  };
+
+  const openModal = async () => {
+    scanForDevices();
+    setIsModalVisible(true);
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.heartRateTitleWrapper}>
+        {connectedDevice ? (
+          <>
+            <PulseIndicator />
+            <Text style={styles.heartRateTitleText}>Anwser</Text>
+            <Text style={styles.heartRateText}>{heartRate} </Text>
+            <TouchableOpacity style={styles.ctaButton} onPress={ () => sendCommandToDevice(connectedDevice,'>QSN<')}>
+              <Text style={styles.ctaButtonText}>{"Send QSN"}</Text>
+            </TouchableOpacity>
+          </>
+        ) : (
+          <Text style={styles.heartRateTitleText}>
+            Please Connect to a Copilot
+          </Text>
+        )}
+      </View>
+      <TouchableOpacity
+        onPress={connectedDevice ? disconnectFromDevice : openModal}
+        style={styles.ctaButton}
+      >
+        <Text style={styles.ctaButtonText}>
+          {connectedDevice ? "Disconnect" : "Connect"}
+        </Text>
+      </TouchableOpacity>
+      <DeviceModal
+        closeModal={hideModal}
+        visible={isModalVisible}
+        connectToPeripheral={connectToDevice}
+        devices={allDevices}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f2f2f2",
+  },
+  heartRateTitleWrapper: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  heartRateTitleText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginHorizontal: 20,
+    color: "black",
+  },
+  heartRateText: {
+    fontSize: 25,
+    marginTop: 15,
+  },
+  ctaButton: {
+    backgroundColor: "#FF6060",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 50,
+    marginHorizontal: 20,
+    marginBottom: 5,
+    borderRadius: 8,
+  },
+  ctaButtonText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
+
+export default App;
---
+++ useBLE.ts
@@ -0,0 +1,220 @@
+/* eslint-disable no-bitwise */
+import { useMemo, useState } from "react";
+import { PermissionsAndroid, Platform } from "react-native";
+// import BluetoothClassic from 'react-native-bluetooth-classic';
+import {
+  BleError,
+  BleManager,
+  Characteristic,
+  Device,
+} from "react-native-ble-plx";
+
+import * as ExpoDevice from "expo-device";
+
+import base64 from "react-native-base64";
+
+const UUID_SERVICE = "0000ffff-0000-1000-8000-00805f9b34fb";
+const UUID_CHARACTERISTIC = "0000ff01-0000-1000-8000-00805f9b34fb";
+
+interface BluetoothLowEnergyApi {
+  requestPermissions(): Promise<boolean>;
+  scanForPeripherals(): void;
+  connectToDevice: (deviceId: Device) => Promise<void>;
+  disconnectFromDevice: () => void;
+  connectedDevice: Device | null;
+  allDevices: Device[];
+  heartRate: String;
+  sendCommandToDevice(device:Device, command:String): Promise<void>;
+}
+
+function useBLE(): BluetoothLowEnergyApi {
+  const bleManager =  useMemo(() => new BleManager(), []);
+  const [allDevices, setAllDevices] = useState<Device[]>([]);
+  const [connectedDevice, setConnectedDevice] = useState<Device | null>(null);
+  const [heartRate, setHeartRate] = useState<String>('');
+
+  const requestAndroid31Permissions = async () => {
+    const bluetoothScanPermission = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+      {
+        title: "Location Permission",
+        message: "Bluetooth Low Energy requires Location",
+        buttonPositive: "OK",
+      }
+    );
+    const bluetoothConnectPermission = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+      {
+        title: "Location Permission",
+        message: "Bluetooth Low Energy requires Location",
+        buttonPositive: "OK",
+      }
+    );
+    const fineLocationPermission = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: "Location Permission",
+        message: "Bluetooth Low Energy requires Location",
+        buttonPositive: "OK",
+      }
+    );
+
+    return (
+      bluetoothScanPermission === "granted" &&
+      bluetoothConnectPermission === "granted" &&
+      fineLocationPermission === "granted"
+    );
+  };
+
+  const requestPermissions = async () => {
+    if (Platform.OS === "android") {
+      if ((ExpoDevice.platformApiLevel ?? -1) < 31) {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          {
+            title: "Location Permission",
+            message: "Bluetooth Low Energy requires Location",
+            buttonPositive: "OK",
+          }
+        );
+        return granted === PermissionsAndroid.RESULTS.GRANTED;
+      } else {
+        const isAndroid31PermissionsGranted =
+          await requestAndroid31Permissions();
+
+        return isAndroid31PermissionsGranted;
+      }
+    } else {
+      return true;
+    }
+  };
+
+  const isDuplicteDevice = (devices: Device[], nextDevice: Device) =>
+    devices.findIndex((device) => nextDevice.id === device.id) > -1;
+
+  const scanForPeripherals = () => {
+    console.log('Iniciando escaneamento de dispositivos...');
+    // const devices = BluetoothClassic.list();
+    // console.log(devices);
+    bleManager.startDeviceScan(null, null, (error, device) => {
+
+      if (error) {
+        console.log('Erro no escaneamento:', error);
+        return;
+      }
+      
+      if (device && device.name?.includes("VIRTEC")) {
+        console.log('Dispositivo encontrado:', device.name);
+        setAllDevices((prevState: Device[]) => {
+          if (!isDuplicteDevice(prevState, device)) {
+            return [...prevState, device];
+          }
+          return prevState;
+        });
+      }
+    });
+  };
+
+  const connectToDevice = async (device: Device) => {
+    try {
+      console.log(`Conectando ao dispositivo ${device.name || device.id}...`);
+    
+      const deviceConnection = await bleManager.connectToDevice(device.id);
+      setConnectedDevice(deviceConnection);
+  
+    
+      const services = await deviceConnection.discoverAllServicesAndCharacteristics();
+    
+      const discoveredServices = await deviceConnection.services();
+      console.log("Serviços descobertos:", discoveredServices);
+  
+      for (const service of discoveredServices) {
+        const characteristics = await service.characteristics();
+        console.log(`Serviço UUID: ${service.uuid}`);
+  
+        for (const characteristic of characteristics) {
+          console.log(` - Característica UUID: ${characteristic.uuid}`);
+        }
+      }
+  
+      bleManager.stopDeviceScan();
+      startStreamingData(deviceConnection);
+  
+    } catch (e) {
+      console.log("Falha ao conectar ou descobrir características:", e);
+    }
+  };
+
+  const disconnectFromDevice = () => {
+    if (connectedDevice) {
+      bleManager.cancelDeviceConnection(connectedDevice.id);
+      setConnectedDevice(null);
+      setHeartRate('');
+    }
+  };
+
+  const onHeartRateUpdate = (
+    error: BleError | null,
+    characteristic: Characteristic | null
+  ) => {
+    if (error) {
+      console.log('onHeartRateUpdate',error);
+      return -1;
+    } else if (!characteristic?.value) {
+      console.log("No Data was recieved");
+      return -1;
+    }
+    console.log('recebendo dados')
+
+    const rawData = base64.decode(characteristic.value);
+    console.log(rawData);
+
+    // if (rawData.includes('RVR')){
+    //   sendCommandToDevice(device)
+    // }
+
+    setHeartRate(rawData);
+  };
+
+  const sendCommandToDevice = async (device: Device, command:string) => {
+    try {
+      // Escreve o comando na característica correta
+      await device.writeCharacteristicWithResponseForService(
+        UUID_SERVICE,
+        UUID_CHARACTERISTIC,
+        base64.encode(command)
+      );
+      console.log("Comando enviado com sucesso!");
+    } catch (e) {
+      console.log("Erro ao enviar comando:", e);
+    }
+  };
+
+
+  const startStreamingData = async (device: Device) => {
+    if (device) {
+      console.log(device);
+      await sendCommandToDevice(device, ">QVR<");
+      device.monitorCharacteristicForService(
+        UUID_SERVICE,
+        UUID_CHARACTERISTIC,
+        onHeartRateUpdate
+      );
+    } else {
+      console.log("No Device Connected");
+    }
+  };
+
+  return {
+    scanForPeripherals,
+    requestPermissions,
+    connectToDevice,
+    allDevices,
+    connectedDevice,
+    disconnectFromDevice,
+    heartRate,
+    sendCommandToDevice
+  };
+}
+
+export default useBLE;
